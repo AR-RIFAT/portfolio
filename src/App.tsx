4b3a5d49a578ab9b1dfa8bfa653e24daf4799732
AR-RIFAT <ar.rifat.954@gmail.com>
2025-09-20 12:50:25 +1000

    enable vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,11 @@ import ExperienceSection from './components/Sections/ExperienceSection';
 import EducationSection from './components/Sections/EducationSection';
 import Footer from './components/Layout/Footer';
 import Snowfall from './components/Snowfall';
+
 export function App() {
-  return <ThemeProvider>
-      <div className="relative min-h-screen w-full bg-sky-200 dark:bg-blue-950 transition-colors duration-300 overflow-x-hidden">
+  return (
+    <ThemeProvider>
+      <div className='relative min-h-screen w-full bg-sky-200 dark:bg-blue-950 transition-colors duration-300 overflow-x-hidden'>
         <Snowfall />
         <Layout>
           <AboutSection />
@@ -19,5 +21,6 @@ export function App() {
           <Footer />
         </Layout>
       </div>
-    </ThemeProvider>;
+    </ThemeProvider>
+  );
 }
--- src/components/ThemeContext.tsx
+++ src/components/ThemeContext.tsx
@@ -1,19 +1,24 @@
 import React, { useEffect, useState, createContext, useContext } from 'react';
+import { Analytics } from '@vercel/analytics/react';
+
 type Theme = 'light' | 'dark';
+
 interface ThemeContextType {
   theme: Theme;
   toggleTheme: () => void;
 }
+
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
 export const ThemeProvider: React.FC<{
   children: React.ReactNode;
-}> = ({
-  children
-}) => {
+}> = ({ children }) => {
   const [theme, setTheme] = useState<Theme>('light');
   useEffect(() => {
     const savedTheme = localStorage.getItem('theme') as Theme | null;
-    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const prefersDark = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    ).matches;
     if (savedTheme) {
       setTheme(savedTheme);
       document.documentElement.classList.toggle('dark', savedTheme === 'dark');
@@ -28,13 +33,19 @@ export const ThemeProvider: React.FC<{
     localStorage.setItem('theme', newTheme);
     document.documentElement.classList.toggle('dark', newTheme === 'dark');
   };
-  return <ThemeContext.Provider value={{
-    theme,
-    toggleTheme
-  }}>
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        toggleTheme
+      }}
+    >
       {children}
-    </ThemeContext.Provider>;
+      <Analytics />
+    </ThemeContext.Provider>
+  );
 };
+
 export const useTheme = (): ThemeContextType => {
   const context = useContext(ThemeContext);
   if (context === undefined) {
